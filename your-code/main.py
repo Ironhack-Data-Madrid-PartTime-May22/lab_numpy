15c7dc5055cbdc43dd0875031cd10d83b8adb120
juanmolera <juan.molera@gmail.com>
2023-05-21 19:04:11 +0200

    [juanmolera](lab-numpy)

--- your-code/main.py
+++ your-code/main.py
@@ -1,68 +1,98 @@
 #1. Import the NUMPY package under the name np.
+import numpy as np
 
 
 
 #2. Print the NUMPY version and the configuration.
+#print(np.__version__)
+#print(np.show_config())
 
 
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
+dimensionesa = (2, 3, 5)
 
+a = np.random.random_sample(dimensionesa)
 
 
 #4. Print a.
+#print(a)
 
 
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
+#dimensionesb = (5, 2, 3)
+dimensionesb = (3, 2, 5)
+
+b = np.ones(dimensionesb)
 
 
 #6. Print b.
+#print(b)
 
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
-
+if a.shape == b.shape:
+    #print('array a and array b have the same size')
+    pass
+else:
+    #print('array a and array b do not have the same size')
+    pass
 
 
 
 #8. Are you able to add a and b? Why or why not?
+# No, their dimensions do not match
 
 
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
+#c = b.T
 
+#print(a.shape)
+#print(c.shape)
+
+c = b.reshape((2,3,5))
 
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
+d = a + c
+
+# Now, dimensions match
 
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
+#print(a)
+#print(d)
 
-
+# Yes... d is a + 1, which is value of c...
 
 #12. Multiply a and c. Assign the result to e.
 
-
+e = a * c
 
 #13. Does e equal to a? Why or why not?
 
-
+# Yes, cause u are multiplying by 1...
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
+d_max = np.max(d)
 
+d_min = np.min(d)
 
+d_mean = np.mean(d)
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
-
+f = np.empty((2, 3, 5), dtype = float)
 
 
 """
