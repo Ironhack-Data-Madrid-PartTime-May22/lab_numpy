d3108b1edc979d56397d06cbf955ee8d2fea6fb5
juanmolera <juan.molera@gmail.com>
2023-05-22 23:21:20 +0200

    [juanmolera](lab-numpy)

--- your-code/main.py
+++ your-code/main.py
@@ -1,62 +1,65 @@
 #1. Import the NUMPY package under the name np.
+
 import numpy as np
 
 
 
 #2. Print the NUMPY version and the configuration.
-#print(np.__version__)
-#print(np.show_config())
+
+print(np.__version__)
+
+print(np.show_config())
 
 
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
+
 dimensionesa = (2, 3, 5)
 
 a = np.random.random_sample(dimensionesa)
 
+primera_forma = np.random.random(dimensionesa)
+segunda_forma = np.random.rand(2, 3, 5)
+tercera_forma = np.random.sample(dimensionesa)
+
 
 #4. Print a.
-#print(a)
+
+print(a)
 
 
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
-#dimensionesb = (5, 2, 3)
-dimensionesb = (3, 2, 5)
+dimensionesb = (5, 2, 3)
+
 
 b = np.ones(dimensionesb)
 
 
 #6. Print b.
-#print(b)
+
+print(b)
 
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
-if a.shape == b.shape:
-    #print('array a and array b have the same size')
-    pass
-else:
-    #print('array a and array b do not have the same size')
-    pass
 
+print(a.size == b.size)
 
 
 #8. Are you able to add a and b? Why or why not?
+
 # No, their dimensions do not match
 
 
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
-#c = b.T
 
-#print(a.shape)
-#print(c.shape)
+c = np.transpose(b, (1,2,0))
 
-c = b.reshape((2,3,5))
 
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
@@ -68,10 +71,10 @@ d = a + c
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
-#print(a)
-#print(d)
+print(a)
+print(d)
 
-# Yes... d is a + 1, which is value of c...
+# Yes... d is a + 1, which is the value of c...
 
 #12. Multiply a and c. Assign the result to e.
 
@@ -82,6 +85,7 @@ e = a * c
 # Yes, cause u are multiplying by 1...
 
 
+
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
 d_max = np.max(d)
@@ -89,6 +93,7 @@ d_max = np.max(d)
 d_min = np.min(d)
 
 d_mean = np.mean(d)
+
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
@@ -105,6 +110,29 @@ In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
 
+for i in range(2):
+
+        for j in range(3):
+
+                for k in range(5):
+
+                        if d[i][j][k] == d_min:
+                                f[i][j][k] = 0
+
+                        elif d[i][j][k] == d_mean:
+                                f[i][j][k] = 50
+
+                        elif d[i][j][k] == d_mean:
+                                f[i][j][k] = 100
+
+                        elif d_min < d[i][j][k] and d[i][j][k] < d_mean:
+                                f[i][j][k] = 25
+
+                        elif d_mean < d[i][j][k] and d[i][j][k] < d_max:
+                                f[i][j][k] = 75
+
+                        else:
+                                pass
 
 
 
@@ -129,6 +157,11 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
 
+print(d)
+
+print(f)
+
+# Yes
 
 """
 #18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values 
@@ -142,3 +175,30 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 Again, you don't need Numpy in this question.
 """
+
+g = np.empty((2, 3, 5), dtype = str)
+
+for i in range(2):
+
+        for j in range(3):
+
+                for k in range(5):
+
+                        if f[i][j][k] == 0 :
+                                g[i][j][k] = 'A'
+
+                        elif f[i][j][k] == 50:
+                                g[i][j][k] = 'C'
+
+                        elif f[i][j][k] == 100:
+                                g[i][j][k] = 'E'
+
+                        elif f[i][j][k] == 25:
+                                g[i][j][k] = 'B'
+
+                        elif f[i][j][k] == 75:
+                                g[i][j][k] = 'D'
+
+                        else:
+                                pass
+
